08eb5624366671e2eeb2d46db43a7c142b1984a1
sre91 <sreems91@gmail.com>
2026-06-03 07:13:27 +0530

    project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">MatchFlow</h1>
+    </div>
+  );
+}
+
+export default App;
