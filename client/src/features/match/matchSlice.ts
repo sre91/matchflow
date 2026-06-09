3211d8687e9b38d97dbd6091b371a1b845155817
sre91 <sreems91@gmail.com>
2026-06-09 12:30:20 +0530

    add and remove match added

--- client/src/features/match/matchSlice.ts
+++ client/src/features/match/matchSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 export type Match = {
   id: number;
@@ -27,8 +28,21 @@ const initialState: MatchState = {
 
 const matchSlice = createSlice({
   name: "match",
+
   initialState,
-  reducers: {},
+
+  reducers: {
+    addMatch: (state, action: PayloadAction<Match>) => {
+      state.matches.push(action.payload);
+    },
+    removeMatch: (state, action: PayloadAction<number>) => {
+      state.matches = state.matches.filter(
+        (match) => match.id !== action.payload,
+      );
+    },
+  },
 });
+
+export const { addMatch, removeMatch } = matchSlice.actions;
 
 export default matchSlice.reducer;
--- client/src/pages/MatchesPage.tsx
+++ client/src/pages/MatchesPage.tsx
@@ -1,23 +1,50 @@
 import MatchCard from "../features/match/components/MatchCard";
-import { useAppSelector } from "../app/hooks";
-import type { Match } from "../features/match/MatchSlice";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { addMatch, removeMatch } from "../features/match/MatchSlice";
 
 function MatchesPage() {
-  const matches: Match[] = useAppSelector((state) => state.match.matches);
+  const dispatch = useAppDispatch();
+
+  const matches = useAppSelector((state) => state.match.matches);
+
+  const handleAddMatch = () => {
+    dispatch(
+      addMatch({
+        id: Date.now(),
+        teamA: "GT",
+        teamB: "SRH",
+      }),
+    );
+  };
 
   return (
     <div>
       <h1 className="mb-6 text-3xl font-bold">🏏 Live Matches</h1>
 
+      <button
+        onClick={handleAddMatch}
+        className="mb-4 rounded bg-blue-500 px-4 py-2 text-white"
+      >
+        Add Match
+      </button>
+
       <div className="grid gap-4">
         {matches.map((match) => (
-          <MatchCard
-            key={match.id}
-            teamA={match.teamA}
-            teamB={match.teamB}
-            score="145/3"
-            overs="16.2"
-          />
+          <div key={match.id}>
+            <MatchCard
+              teamA={match.teamA}
+              teamB={match.teamB}
+              score="145/3"
+              overs="16.2"
+            />
+
+            <button
+              onClick={() => dispatch(removeMatch(match.id))}
+              className="mt-2 rounded bg-red-500 px-3 py-1 text-white"
+            >
+              Delete Match
+            </button>
+          </div>
         ))}
       </div>
     </div>
