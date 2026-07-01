ac6d9bcd088096bf850f81609d99bcc41f8fe4c9
sre91 <sreems91@gmail.com>
2026-07-01 11:47:05 +0530

    auth and authrization completed

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from "express";
-import User from "../models/User";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({
-      email,
-    });
+    const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       return res.status(409).json({
@@ -24,10 +23,51 @@ export const registerUser = async (req: Request, res: Response) => {
       password: hashedPassword,
     });
 
-    res.status(201).json(user);
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
   } catch (error) {
     res.status(500).json({
       message: "Registration failed",
+    });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET as string,
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Login failed",
     });
   }
 };
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/authController";
+import { loginUser, registerUser } from "../controllers/authController";
 
 const router = Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
